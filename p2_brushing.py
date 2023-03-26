2630f916ea296b9ee8d0a28a29990d840bb8c813
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-03-26 13:59:33 +0700

    Update p2_brushing.py
    Signed-off-by: nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>

--- p2_brushing.py
+++ p2_brushing.py
@@ -1,14 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sn
 import plotly.express as px
 import streamlit as st
 
 with st.sidebar:
-      X = st.selectbox(
-      'X',
-      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
-      Y = st.selectbox(
-      'Y',
-      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
       Size = st.selectbox(
       'Size',
       ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
@@ -17,23 +14,51 @@ with st.sidebar:
       ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
       Circle_area = st.slider('Circle Area', 0, 100, 60)
 
-      st.write('X:', X)
-      st.write('Y:', Y)
       st.write('Size:', Size)
       st.write('Color:', Color)
       st.write("Circle Area:", Circle_area)
 
-Data = pd.read_csv('https://raw.githubusercontent.com/nakhwaazizah/visdat/main/factbook.csv',index_col=0)
+Data = pd.read_csv(r"C:\Users\Puteri Setyo Utami\Downloads\factbook.csv")
 
-fig = px.scatter(data_frame=Data,x=X, y=Y,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
-
-#st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-
-col1, col2 = st.columns([10, 10])
+col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
 
 with col1:
+      X1 = st.selectbox(
+      'X',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'), key='Chart1')
+      Y1 = st.selectbox(
+      'Y',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'), key='Chart2')
+      fig = px.scatter(data_frame=Data,x=X1, y=Y1,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
       st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
 with col2:
+      X2 = st.selectbox(
+      'X',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'), key='Chart3')
+      Y2 = st.selectbox(
+      'Y',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'),  key='Chart4')
+      fig = px.scatter(data_frame=Data,x=X2, y=Y2,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
       st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
+with col3:
+      X3 = st.selectbox(
+      'X',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'), key='Chart5')
+      Y3 = st.selectbox(
+      'Y',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'),  key='Chart6')
+      fig = px.scatter(data_frame=Data,x=X3, y=Y3,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
+      st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
+with col4:
+      X4 = st.selectbox(
+      'X',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'), key='Chart7')
+      Y4 = st.selectbox(
+      'Y',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'), key='Chart8')
+      fig = px.scatter(data_frame=Data,x=X4, y=Y4,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
+      st.plotly_chart(fig, theme="streamlit", use_container_width=True)
