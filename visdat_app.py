0dce4257054c4d04f1ef0ad45c2555d44508b776
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-03-26 12:58:33 +0700

    Update visdat_app.py
    Signed-off-by: nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>

--- visdat_app.py
+++ visdat_app.py
@@ -23,9 +23,8 @@ with st.sidebar:
       st.write('Color:', Color)
       st.write("Circle Area:", Circle_area)
 
-Data = pd.read_csv(r"C:\Users\Lenovo\Downloads\factbook.csv")
+Data = pd.read_csv('https://raw.githubusercontent.com/nakhwaazizah/visdat/main/factbook.csv',index_col=0)
 
 fig = px.scatter(data_frame=Data,x=X, y=Y,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
 
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-
