a700598a50c04df0b2d564a43fc4d8769df447a9
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-03-26 13:42:25 +0700

    Update Brushing.py
    Signed-off-by: nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>

--- Brushing.py
+++ Brushing.py
@@ -1,1 +1,37 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+with st.sidebar:
+      X = st.selectbox(
+      'X',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
+      Y = st.selectbox(
+      'Y',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
+      Size = st.selectbox(
+      'Size',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
+      Color = st.selectbox(
+      'Color',
+      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
+      Circle_area = st.slider('Circle Area', 0, 100, 60)
+
+      st.write('X:', X)
+      st.write('Y:', Y)
+      st.write('Size:', Size)
+      st.write('Color:', Color)
+      st.write("Circle Area:", Circle_area)
+
+Data = pd.read_csv('https://raw.githubusercontent.com/nakhwaazizah/visdat/main/factbook.csv',index_col=0)
+
+fig = px.scatter(data_frame=Data,x=X, y=Y,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
+
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+col1, col2 = st.columns([4, 1])
+
+col1.subheader('Brushing')
+col1.st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+col2.st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
